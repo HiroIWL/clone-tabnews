d77bcfeacd327eb5b68bfd23ac5d55461c0db06b
Artur Iwasaki Lourenço <hiroiwl@gmail.com>
2024-10-07 16:01:37 -0300

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Estudo de programação usando o TabNews como referência
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-    return <h1>Estou estudando principalmente para mim, mas com certeza também estou fazendo isso por nós. sz</h1>
+  return (
+    <h1>
+      Estou estudando principalmente para mim, mas com certeza também estou
+      fazendo isso por nós. sz
+    </h1>
+  );
 }
 
 export default Home;
